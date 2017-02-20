6f1b8839804a3589bbaec576590ae515eedf261d
Rain1368189893 <1368189893@qq.com>
2017-02-20 23:39:42 +0800

    add test router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@
  * Created by hanan on 16/10/15.
  */
 const Hello = r => require(['views/hello'], r);
+const Test = r => require(['views/test'], r);
 const NotFound = r => require(['views/notfound'], r);
 
 // 根目录
@@ -11,7 +12,8 @@ const rootPath = '';
 // 页面路由
 const routes = [
   {path: '', redirect: {name: 'hello'}},
-  {path: '/hello', component: Hello, name: 'hello'}
+  {path: '/hello', component: Hello, name: 'hello'},
+  {path: '/test', component: Test, name: 'test'},
 ].map(route => {
   route.path = rootPath + route.path;
   return route;
---
+++ src/views/test.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <c-title :text="title"></c-title>
+    <p class="welcome">这是一个测试路由</p>
+    <div v-html="content"></div>
+  </div>
+</template>
+
+<script>
+
+  import {mapState} from 'vuex';
+  import cTitle from 'components/title';
+
+  export default {
+    data () {
+      return {
+        title: 'This is a test page!',
+        content: ''
+      }
+    },
+    methods: {
+      async getContent () {
+        const response = await fetch('/api/hello');
+        this.content = await response.text();
+      }
+    },
+    mounted () {
+      this.$store.commit('MESSAGE', '欢迎使用 vue！');
+      this.getContent();
+    },
+
+    components: {cTitle}
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" rel="stylesheet/scss" scoped>
+  h1 {
+    color: #42b983;
+  }
+
+  .logo {
+    width: 100px;
+    height: 100px;
+    a {
+      color: #42b983;
+      text-decoration: none;
+    }
+  }
+</style>
